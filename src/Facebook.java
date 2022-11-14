19007538fcc5d52fb85f04524944b08db78798f4
rifkofebryalaziz <rifkofebryalaziz30@gmail.com>
2022-11-14 18:28:45 +0700

    1B

---
+++ src/Facebook.java
@@ -0,0 +1,28 @@
+public class Facebook {
+    private int UserID;
+    private int UserNameUNique;
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(int userID) {
+        UserID = userID;
+    }
+
+    public int getUserNameUNique() {
+        return UserNameUNique;
+    }
+
+    public void setUserNameUNique(int userNameUNique) {
+        UserNameUNique = userNameUNique;
+    }
+    public Fecebook(int UserID,int UserNameUnique){
+        this.UserID =
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
